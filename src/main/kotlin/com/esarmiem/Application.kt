b804fd6bbd1c5798a09440e376ddb2dd1e3ca3ef
elder sarmiento <eldersarmiento1@gmail.com>
2023-11-13 15:15:06 -0500

    Initial commit

---
+++ src/main/kotlin/com/esarmiem/Application.kt
@@ -0,0 +1,16 @@
+package com.esarmiem
+
+import com.esarmiem.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
